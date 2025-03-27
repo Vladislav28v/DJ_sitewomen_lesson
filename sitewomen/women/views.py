9c4a559088bce4956f12fd9055020527abe9a893
Vladislav <vykurinnoy@gmail.com>
2025-03-27 20:50:01 +0300

    Добавления шаблона post.html

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404, HttpResponseRedirect, HttpResponsePermanentRedirect
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 from django.template.loader import render_to_string
 from django.template.defaultfilters import slugify
+
+from .models import Women
 
 menu = [{'title': "О сайте", 'url_name': 'about'},
         {'title': "Добавить статью", 'url_name': 'add_page'},
@@ -40,7 +42,14 @@ def about(request):
 
 
 def show_post(request, post_id):
-    return HttpResponse(f"Отображение статьи с id = {post_id}")
+    post = get_object_or_404(Women, pk=post_id)
+    data = {
+        'title': post.title,
+        'menu': menu,
+        'posts': post,
+        'cat_selected': 1,
+    }
+    return render(request, 'women/post.html', data)
 
 
 def addpage(request):
